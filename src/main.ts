f751a14c0af842be94b03a54b1ff5eb9587becdd
Yogesh Pal <yogeshpal5049@gmail.com>
2025-11-03 16:03:58 +0530

    done

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-   origin: ['https://frontendrepo-lake.vercel.app','http://localhost:3000'],
+   origin: ['https://frontendrepo-three.vercel.app','http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
